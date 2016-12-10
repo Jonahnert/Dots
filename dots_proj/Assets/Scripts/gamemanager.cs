d824a07b0ad7d84d1b7a4a1d62b3423f9c3821e2
artteam <whalehousestudios@gmail.com>
2016-12-10 14:24:53 -0500

    Roadtrip work
    tried to reconcile look-at and colliders

--- dots_proj/Assets/Scripts/gamemanager.cs
+++ dots_proj/Assets/Scripts/gamemanager.cs
@@ -15,4 +15,18 @@ public class gamemanager : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public void SwitchTurn(string turnID)
+    {
+        if (turnID == "P1")
+        {
+            turnTaker = "P2";
+            Debug.Log("Player 2 turn");
+        }
+        else if (turnID == "P2")
+        {
+            turnTaker = "P1";
+            Debug.Log("Player 1 turn");
+        }
+    }
 }
--- dots_proj/Assets/Scripts/node.cs
+++ dots_proj/Assets/Scripts/node.cs
@@ -12,6 +12,9 @@ public class node : MonoBehaviour {
         GMScript = GameManager.GetComponent<gamemanager>();
     }
     private RaycastHit2D[] linesHit;
+    /// <summary>
+    /// 
+    /// </summary>
     void OnMouseDown()
     {
         if (isUsed == false)
@@ -20,7 +23,14 @@ public class node : MonoBehaviour {
             //RaycastHit hit;
             if(GMScript != null)
             {
-                linesHit = Physics2D.LinecastAll(new Vector2(GMScript.P1Start.position.x, GMScript.P1Start.position.y), new Vector2(transform.position.x, transform.position.y));
+                if (GMScript.turnTaker == "P1")
+                {
+                    linesHit = Physics2D.LinecastAll(new Vector2(GMScript.P1Start.position.x, GMScript.P1Start.position.y), new Vector2(transform.position.x, transform.position.y));
+                }
+                else if (GMScript.turnTaker == "P2")
+                {
+                    linesHit = Physics2D.LinecastAll(new Vector2(GMScript.P2Start.position.x, GMScript.P2Start.position.y), new Vector2(transform.position.x, transform.position.y));
+                }
                 foreach (RaycastHit2D hit in linesHit)
                 {
                     // layer 8 is for lines
@@ -41,25 +51,56 @@ public class node : MonoBehaviour {
                     else if (hit.collider.gameObject.layer == 9)
                     {
                         hit.collider.gameObject.GetComponent<node>().isUsed = true;
+                        Debug.Log("score ONE NODE");
                         // score 1 for opponent
                     }
                 }
-                float xDistance = GMScript.P1Start.position.x + transform.position.x;
-                float yDistance = GMScript.P1Start.position.y + transform.position.y;
+                //these are used to find the midpoint at which to spawn and stretch the line object
+                float xDistance = 0f;
+                float yDistance = 0f;
+                float lineScale = 0f;
+                if (GMScript.turnTaker == "P1")
+                {
+                    xDistance = GMScript.P1Start.position.x + transform.position.x;
+                    yDistance = GMScript.P1Start.position.y + transform.position.y;
+                    lineScale = Vector3.Distance(GMScript.P1Start.position, transform.position);
+                }
+                else if (GMScript.turnTaker == "P2")
+                {
+                    xDistance = GMScript.P2Start.position.x + transform.position.x;
+                    yDistance = GMScript.P2Start.position.y + transform.position.y;
+                    lineScale = Vector3.Distance(GMScript.P2Start.position, transform.position);
+                }
+
                 Vector2 midpoint = new Vector2((xDistance) / 2f, (yDistance) / 2f);
                 GameObject newLine = (GameObject)Instantiate(GMScript.lineObj, midpoint, Quaternion.identity);
                 newLine.transform.LookAt(transform);
-                float lineScale = Vector3.Distance(GMScript.P1Start.position, transform.position);
-                    //18.34f * Mathf.Sqrt(Mathf.Pow(xDistance, 2) + Mathf.Pow(yDistance, 2));
-                newLine.transform.localScale = new Vector3(1, 1, lineScale);
+                //newLine.transform.Rotate(0f, 90f, 0);
+                //newLine.transform.eulerAngles = new Vector3(newLine.transform.rotation.x, newLine.transform.rotation.y, newLine.transform.rotation.z);
+                //newLine.transform.eulerAngles = new Vector3(newLine.transform.rotation.x, 180, newLine.transform.rotation.z);
+                if (GMScript.turnTaker == "P1") 
+                {
+                    newLine.GetComponent<lineScript>().owner = "P1";
+                }
+                else if (GMScript.turnTaker == "P2")
+                {
+                    newLine.GetComponent<lineScript>().owner = "P2";
+                }
+                //18.34f * Mathf.Sqrt(Mathf.Pow(xDistance, 2) + Mathf.Pow(yDistance, 2));
+
+                //-1 in the x scale is to compensate for the width of the nodes
+                newLine.transform.localScale = new Vector3(lineScale-1, newLine.transform.localScale.y, newLine.transform.localScale.z);
                 if (GMScript.turnTaker == "P1")
                 {
                     GMScript.P1Start = gameObject.transform;
+                    GMScript.SwitchTurn("P1");
                 }
                 else if (GMScript.turnTaker == "P2")
                 {
                     GMScript.P2Start = gameObject.transform;
+                    GMScript.SwitchTurn("P2");
                 }
+         
             }
         }
 
